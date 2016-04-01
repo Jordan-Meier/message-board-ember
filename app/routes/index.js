0e2e05dda505fd3e99f9eb853b1a0e06c51fbd50
Jordan Meier <jbirdmeier@gmail.com>
2016-04-01 08:49:22 -0700

    add saveQuestion function to index route handler and add full functionality for creating new questions

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
+  },
+  actions: {
+    saveQuestion(newInputs) {
+      var newQuestion = this.store.createRecord('question', newInputs);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
   }
 });
